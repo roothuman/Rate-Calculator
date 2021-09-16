0d0ec01fa3de01276a2a2210b2e1feace1880a58
Moses Abah <amoses@swiftHQ.com>
2021-09-16 13:28:16 +0100

    Add project files.

---
+++ Calculate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_5
+{
+    class Calculate
+    {
+        private const int Megabyte = 1024;
+        private double Rate;
+        public double CalculateRate(double price, double volume)
+        {
+            Rate = price / (volume * Megabyte);
+            return Rate;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Week_5
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+          
+            try
+            {
+                Console.WriteLine("Enter Price");
+                var price = double.Parse(Console.ReadLine() ?? string.Empty);
+
+                Console.WriteLine("\nEnter Volume");
+                var volume =double.Parse( Console.ReadLine() ?? string.Empty);
+
+                while (true)
+                {
+
+                    if (volume <= 0)
+                    {
+                        Console.WriteLine("\nVolume can only be greater than zero");
+                        Console.WriteLine("\nEnter valid volume or press 'q' to quit");
+
+                        var input = Console.ReadLine();
+                        if (input == "q")
+                        {
+                            Console.WriteLine("\n**ENDED**");
+                            break;
+                        }
+                        
+                        volume = double.Parse(input ?? string.Empty);
+                    }
+
+                    else
+                    {
+                        var r = new Calculate();
+
+                        var rate = r.CalculateRate(price, volume);
+
+                        Console.WriteLine($"\nThe Rate is: {rate:N5}");
+                        break;
+                    }
+                }
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("\nInvalid Input");
+            }
+        }
+    }
+}
